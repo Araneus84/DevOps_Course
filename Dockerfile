6f0556085aee9cf54d521a3f5a0bb34246835154
Alex <alexanderk84@gmail.com>
2025-06-28 00:01:06 +0300

    Updated compose to use build and my docker hub image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM python:3.14.0b1-alpine3.21
+FROM python:3.11-slim-buster
 
 WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-COPY *.py .
+COPY . .
+
+RUN mkdir -p /app/logs
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+import os
 import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    random_number = random.randint(0, 100)
-    return f'Hello, World! #{random_number}'
+    app_env = os.environ.get("APP_ENV", "default")
+    random_number = random.randint(1, 100)
+    return f"Hello, World! #{random_number} - Environment: {app_env}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
